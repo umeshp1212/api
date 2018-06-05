d85aa9adc1e3a532f4b61c241ddc35a877ee7061
Umesh Patil <umeshpatil@della.in>
2018-06-05 14:19:19 +0530

    Created Model -a

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/products','ProductController');
+
+Route::group(['prefix' => 'product'], function(){
+    Route::apiResource('/{products}/reviews','ReviewController');
+});
